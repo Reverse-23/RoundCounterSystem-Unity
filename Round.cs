68051fd56b45c33bb1c05a92220f1e1b6a8fb3f0
Reverse-23 <81431424+Reverse-23@users.noreply.github.com>
2021-03-26 21:52:07 +0100

    Update Round.cs

--- Round.cs
+++ Round.cs
@@ -1,9 +1,10 @@
 public class Roudns : MonoBehaviour
 {
     // Vaiables
+    //Audio source it's if you want to play audio while round transiction
 
     public Text Roundtxt;
-    public AudioSource audioSource;
+    //public AudioSource audioSource;
     [SerializeField]private string Roundstring;
     [SerializeField]private float NextRoundTime;
 
@@ -29,6 +30,12 @@ public class Roudns : MonoBehaviour
         {
             CurrentRound += 1;
             CurrentTime = 0;
+            
+            //if (CurrentRound >= 2)
+            //{
+            //    audioSource.Play();
+           // }
+
 
         }
     }
